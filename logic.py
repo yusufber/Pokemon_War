397c029737307172bbaf737bf4cdcdaad72850fb
Yusuf Berk Demirel <138680780+yusufber@users.noreply.github.com>
2025-10-05 19:37:31 +0300

    sa
    sa

---
+++ logic.py
@@ -0,0 +1,158 @@
+import aiohttp  # Eşzamansız HTTP istekleri için bir kütüphane
+import random
+import asyncio 
+from datetime import datetime, timedelta
+
+class Pokemon:
+    pokemons = {}
+    # Nesne başlatma (kurucu)
+    def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.name = None
+        if pokemon_trainer not in Pokemon.pokemons:
+            Pokemon.pokemons[pokemon_trainer] = self
+        else:
+            self = Pokemon.pokemons[pokemon_trainer]
+
+    async def get_name(self):
+        # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+            async with session.get(url) as response:  # GET isteği gönderme
+                if response.status == 200:
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    return data['forms'][0]['name']  #  Pokémon adını döndürme
+                else:
+                    return "Pikachu"  # İstek başarısız olursa varsayılan adı döndürür
+
+    async def info(self):
+        # Pokémon hakkında bilgi döndüren bir metot
+        if not self.name:
+            self.name = await self.get_name()# Henüz yüklenmemişse bir adın geri alınması
+            self.attack=await self.get_attack()
+            self.hp=await self.get_hp()
+            self.defense=await self.get_defense()
+            
+        return f"Pokémonunuzun ismi: {self.name}"  # Pokémon adını içeren dizeyi döndürür
+
+    async def show_img(self):
+        # PokeAPI aracılığıyla bir pokémon görüntüsünün URL'sini almak için asenktron metot
+        # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+            async with session.get(url) as response:  # GET isteği gönderme
+                if response.status == 200:
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    return data['sprites']['front_default']  #  Pokémon adını döndürme
+                else:
+                    return "Pikachu"  # İstek başarısız olursa varsayılan adı döndürür
+                
+
+    async def get_hp(self):
+        # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+            async with session.get(url) as response:  # GET isteği gönderme
+                if response.status == 200:
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    return data['stats'][0]['base_stat']  #  Pokémon adını döndürme
+                else:
+                    return 50  # İstek başarısız olursa varsayılan adı döndürür
+    
+
+
+
+    async def get_attack(self):
+        # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+            async with session.get(url) as response:  # GET isteği gönderme
+                if response.status == 200:
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    return data['stats'][1]['base_stat']  #  Pokémon adını döndürme
+                else:
+                    return 50  # İstek başarısız olursa varsayılan adı döndürür
+                
+
+
+
+    async def get_defense(self):
+        # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+            async with session.get(url) as response:  # GET isteği gönderme
+                if response.status == 200:
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    return data['stats'][2]['base_stat']  #  Pokémon adını döndürme
+                else:
+                    return 50  # İstek başarısız olursa varsayılan adı döndürür
+                
+    async def feed(self, feed_interval=20, hp_increase=10):
+        current_time = datetime.now()
+        delta_time = timedelta(seconds=feed_interval)
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Pokémon sağlığı geri yüklenir. Mevcut sağlık: {self.hp}"
+        else:
+            return f"Pokémonunuzu şu zaman besleyebilirsiniz: {current_time+delta_time}"
+
+    async def saldir(self,enemy):
+        if self.hp <= 0:
+            return f'{self.name} dinleniyor'
+
+        else:
+            if isinstance(enemy , Wizard):  # Düşmanın Wizard veri tipi olup olmadığının kontrol edilmesi (Sihirbaz sınıfının bir örneği midir?) 
+                sans = random.randint(1, 4) 
+                if sans == 1:
+                    return f"{enemy.name}, savaşta bir kalkan kullandı!"
+
+            hasar=round(self.attack*(enemy.defense/(enemy.defense+100)),2)
+            if enemy.hp <= hasar:
+                enemy.hp=0
+                return f'{self.name} {enemy.name}\'e saldırdı.\n {enemy.name} yenildi.'
+
+            else:
+                enemy.hp-=hasar
+                return f'{self.name} {enemy.name}\'e saldırdı.{hasar} verdi.\n{enemy.name}\'in canı {enemy.hp} kaldı '
+        
+
+
+class Fighter(Pokemon):
+    async def saldir(self, enemy):
+        super_guc = random.randint(10 , 20)  
+        self.attack += super_guc
+        sonuc = await super().saldir(enemy)  
+        self.attack -= super_guc
+        return sonuc + f"\n{self.name} süper saldırı kullandı. Eklenen güç: {super_guc}"
+    
+
+
+
+
+
+class Wizard(Pokemon):
+    async def feed(self, feed_interval=20, hp_increase=10):
+        feed_interval -= random.randint(1, 15)
+        return await super().feed(feed_interval, hp_increase)
+
+
+
+
+
+if __name__== '__main__':
+    pokemon1=Wizard('Rüzgar')
+    pokemon2=Fighter('Ege')
+    async def deneme():
+        print(await pokemon1.info())
+        print(await pokemon2.info())
+        print("----------------------------------------------------------------------------------------------------------")
+        print(await pokemon1.saldir(pokemon2))
+        print('------------------------------------------------------------------------------------------------------------')
+        print(await pokemon2.saldir(pokemon1))
+
+
+
+
+    asyncio.run(deneme())
---
+++ main.py
@@ -0,0 +1,85 @@
+import discord
+from discord.ext import commands
+from config import token
+from logic import Pokemon,Fighter,Wizard
+import random
+
+# Bot için yetkileri/intents ayarlama
+intents = discord.Intents.all()  # Varsayılan ayarların alınması
+
+# Tanımlanmış bir komut önekine ve etkinleştirilmiş amaçlara sahip bir bot oluşturma
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Bot çalışmaya hazır olduğunda tetiklenen bir olay
+@bot.event
+async def on_ready():
+    print(f'Giriş yapıldı:  {bot.user.name}')  # Botun adını konsola çıktı olarak verir
+
+# '!go' komutu
+@bot.command()
+async def go(ctx):
+    author = ctx.author.name  # Mesaj yazarının adını alma
+    # Kullanıcının zaten bir Pokémon'u olup olmadığını kontrol edin. Eğer yoksa, o zaman...
+    if author not in Pokemon.pokemons.keys():
+        psec=random.randint(1,3)
+        if psec == 1:
+            pokemon = Pokemon(author)  # Yeni bir Pokémon oluşturma
+            await ctx.send('Size Sıradan Pokemon geldi')
+
+        if psec == 2:
+            pokemon = Fighter(author)  # Yeni bir Pokémon oluşturma
+            await ctx.send('Size Fighter Pokemon geldi')
+
+        if psec == 3:
+            pokemon = Wizard(author)  # Yeni bir Pokémon oluşturma
+            await ctx.send('Size Wizrad Pokemon geldi')
+
+        await ctx.send(await pokemon.info())  # Pokémon hakkında bilgi gönderilmesi
+        image_url = await pokemon.show_img()  # Pokémon resminin URL'sini alma
+        if image_url:
+            embed = discord.Embed()  # Gömülü mesajı oluşturma
+            embed.set_image(url=image_url)  # Pokémon'un görüntüsünün ayarlanması
+            await ctx.send(embed=embed)  # Görüntü içeren gömülü bir mesaj gönderme
+        else:
+            await ctx.send("Pokémonun görüntüsü yüklenemedi!")
+    else:
+        await ctx.send("Zaten kendi Pokémonunuzu oluşturdunuz!")  # Bir Pokémon'un daha önce oluşturulup oluşturulmadığını gösteren bir mesaj
+
+@bot.command()
+async def feed(ctx):
+    author = ctx.author.name
+    if author not in Pokemon.pokemons.keys():
+        await ctx.send("😕 Pokémon'un yok!")
+    else:
+        poke = Pokemon.pokemons[author]
+        await poke.feed()  # Eğer feed async ise await ile çağırılır
+        await ctx.send(await poke.feed())  # Besleme sonucunu gönder
+
+
+@bot.command()
+async def sil(ctx):
+    author = ctx.author.name
+    if author not in Pokemon.pokemons.keys():
+        await ctx.send ('Zaten sende Pokemon yok')
+    else:
+        del Pokemon.pokemons[author]
+        await ctx.send('Pokemon Silindi Yeni Alabilirsin')
+
+@bot.command()
+async def attack(ctx):
+    target = ctx.message.mentions[0] if ctx.message.mentions else None  # Mesajda belirtilen kullanıcıyı alırız
+    if target:  # Kullanıcının belirtilip belirtilmediğini kontrol ederiz
+        # Hem saldırganın hem de hedefin Pokémon sahibi olup olmadığını kontrol ederiz
+        if target.name in Pokemon.pokemons and ctx.author.name in Pokemon.pokemons:
+            enemy = Pokemon.pokemons[target.name]  # Hedefin Pokémon'unu alırız
+            attacker = Pokemon.pokemons[ctx.author.name]  # Saldırganın Pokémon'unu alırız
+            result = await attacker.saldir(enemy)  # Saldırıyı gerçekleştirir ve sonucu alırız
+            await ctx.send(result)  # Saldırı sonucunu göndeririz
+        else:
+            await ctx.send("Savaş için her iki tarafın da Pokémon sahibi olması gerekir!")  # Katılımcılardan birinin Pokémon'u yoksa bilgilendiririz
+    else:
+        await ctx.send("Saldırmak istediğiniz kullanıcıyı etiketleyerek belirtin.")  # Saldırmak için kullanıcıyı etiketleyerek belirtmesini isteriz
+
+
+
+bot.run(token)
